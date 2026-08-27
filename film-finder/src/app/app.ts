e643263780dc7e3a7031c5ee0155bf98f001715a
Luke Stevens <lukestevens100@gmail.com>
2026-08-27 23:19:40 +0100

    taskbar added

--- film-finder/src/app/app.ts
+++ film-finder/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Taskbar } from './common-components/taskbar/taskbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Taskbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('film-finder');
+  username = "placeholder";
 }
