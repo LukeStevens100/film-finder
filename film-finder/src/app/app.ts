c0057fd4c22b92dc464ff0b2ab304f3332324e2c
Luke Stevens <lukestevens100@gmail.com>
2026-08-28 10:28:21 +0100

    added footer

--- film-finder/src/app/app.ts
+++ film-finder/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Taskbar } from './common-components/taskbar/taskbar';
+import { Footer } from './common-components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Taskbar],
+  imports: [Taskbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
